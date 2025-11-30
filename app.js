1b6cc822fcf09afe7291e6c3f6dd0cd46e944c7f
Jatin Kansal <jatin.kansal@oriserve.com>
2025-11-30 18:34:46 +0530

    Addition : morgan and cors packages, vercel configurations

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const path = require('path');
+const morgan = require('morgan')
+const cors = require('cors')
 const urlRoutes = require('./routes/urlRoutes');
 const dotenv = require('dotenv')
 dotenv.config()
@@ -8,6 +10,8 @@ const app = express();
 
 // Middleware
 app.use(express.json());
+app.use(cors())
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms'))
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes (API + redirect)
